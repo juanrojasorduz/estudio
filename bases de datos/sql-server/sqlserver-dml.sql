ba2f2cf393efcd48cacd7432d995f522410774d5
juandiegorojas <jdrojaso@hotmail.com>
2022-10-27 20:16:17 -0500

    ajustes

---
+++ bases de datos/sql-server/sqlserver-dml.sql
@@ -0,0 +1,21 @@
+--- DATA MANIPULATION LANGUAGE (DML) -> Modificar el contenido de la base de datos
+--  SENTENCIAS BASICAS:-
+--  *INSERT
+--  *UPDATE
+--  *DELETE
+--  *SELECT
+--  *MERGE
+	
+INSERT INTO platziblog.people (person_id ,last_name, first_name, address, city) 
+VALUES (4,'Hernandez', 'Laura', 'Calle 21', 'Monterrey');
+
+;
+
+update platziblog.people 
+set last_name = 'Chavez',city = 'Bogotá'
+where person_id = 1 ;
+
+delete from platziblog.people 
+where person_id = 4;
+
+select * from platziblog.people where city = 'Monterrey'
