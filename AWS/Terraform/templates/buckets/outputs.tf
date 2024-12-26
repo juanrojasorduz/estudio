279cb4633ac414475283c1c072e4aa71a9302f45
dsc-orojas <rojas.juan@exlservice.com>
2024-12-26 22:33:58 +0000

    templates

---
+++ AWS/Terraform/templates/buckets/outputs.tf
@@ -0,0 +1,15 @@
+output "bucket_one_name" {
+  value = module.bucket_one.bucket_name
+}
+
+output "bucket_two_name" {
+  value = module.bucket_two.bucket_name
+}
+
+output "bucket_three_name" {
+  value = module.bucket_three.bucket_name
+}
+
+output "bucket_four_name" {
+  value = module.bucket_four.bucket_name
+}
