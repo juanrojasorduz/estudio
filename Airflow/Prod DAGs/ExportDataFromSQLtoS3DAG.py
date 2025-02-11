c0673e2898d268b8ad40cee5219fa372c2ada615
juandiegorojas <jdrojaso@hotmail.com>
2025-02-11 10:36:22 -0500

    adjustments

--- Airflow/Prod DAGs/ExportDataFromSQLtoS3DAG.py
+++ Airflow/Prod DAGs/ExportDataFromSQLtoS3DAG.py
@@ -2,17 +2,26 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.operators.dummy import DummyOperator
 from airflow import DAG
 from datetime import datetime
+from airflow.providers.amazon.aws.transfers.sql_to_s3 import SqlToS3Operator
 
 default_args = {
     'retries': 1,
 }
 
-with DAG(dag_id='InsertRecordsInTableDAG', start_date=datetime(2023, 1, 1), default_args=default_args, schedule='@daily', catchup=False):
+with DAG(dag_id='ExportDataFromSQLtoS3DAG', start_date=datetime(2023, 1, 1), default_args=default_args, schedule='@daily', catchup=False):
 
     start = DummyOperator(task_id='DuStartOp')
 
-    create_table_country = SQLExecuteQueryOperator(
-        task_id="CreateTableCountry",
+    drop_table_if_exists = SQLExecuteQueryOperator(
+        task_id="DropTableIfExists",
+        conn_id="mssql_default",
+        sql=r"""
+        DROP TABLE IF EXISTS master.dbo.Country;
+        """
+    )    
+
+    create_table = SQLExecuteQueryOperator(
+        task_id="CreateTable",
         conn_id="mssql_default",
         sql=r"""
         CREATE TABLE Country (
@@ -23,8 +32,8 @@ with DAG(dag_id='InsertRecordsInTableDAG', start_date=datetime(2023, 1, 1), defa
         """
     )
 
-    insert_countries_table = SQLExecuteQueryOperator(
-        task_id="InsertCountriesTable",
+    insert_records_in_table = SQLExecuteQueryOperator(
+        task_id="InsertRecordsInTable",
         conn_id="mssql_default",
         sql=r"""
                 INSERT INTO Country (name, continent)
@@ -38,6 +47,16 @@ with DAG(dag_id='InsertRecordsInTableDAG', start_date=datetime(2023, 1, 1), defa
                 """
     )
 
+    export_data_to_s3 = SqlToS3Operator(
+        task_id="ExportDataToS3",
+        sql_conn_id="mssql_default",
+        aws_conn_id="aws_s3",
+        query="SELECT * FROM master.dbo.Country;",
+        s3_bucket="bucketfordatajds3",
+        s3_key="airflow/countries.csv",
+        replace=True
+    )
+
     end = DummyOperator(task_id='DuEndOp', trigger_rule='none_failed')
 
-    start >> create_table_country >> insert_countries_table >> end
+    start >> drop_table_if_exists >> create_table >> insert_records_in_table >> export_data_to_s3 >> end
